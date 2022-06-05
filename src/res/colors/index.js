206442be65997c478f4fb8e43295c7dff7f49ac9
Raihan Sarkar <bdyoungdeveloper@gmail.com>
2022-06-05 15:08:55 +0600

    react native custom font added

---
+++ src/res/colors/index.js
@@ -0,0 +1,18 @@
+const mainColors = {
+  cyan: '#06b6d4',
+  black: '#00000',
+  white: '#ffffff',
+  grey: '#8D92A3',
+  peace: '#a4b0be',
+  lightGrey: 'rgba(196, 196, 196, 0.5)',
+  darkGreen: '#275A02',
+};
+export const colors = {
+  primary: mainColors.cyan,
+  black: mainColors.black,
+  white: mainColors.white,
+  grey: mainColors.grey,
+  peace: mainColors.peace,
+  lightGrey: mainColors.lightGrey,
+  darkGreen: mainColors.darkGreen,
+};
--- src/screens/GetStarted/index.js
+++ src/screens/GetStarted/index.js
@@ -1,6 +1,8 @@
 import {StyleSheet, Text, View, Image} from 'react-native';
 import React from 'react';
 import {DoctorImg} from '../../res/images/Illustrations/inex';
+import {colors} from '../../res/colors';
+import {fonts} from '../../res/fonts';
 
 const GetStarted = () => {
   return (
@@ -10,6 +12,18 @@ const GetStarted = () => {
         <Text style={styles.titleText}>Booking Your Doctor</Text>
         <Text style={styles.titleText}>Online</Text>
       </View>
+      <View
+        style={{
+          marginTop: 20,
+          backgroundColor: colors.primary,
+          height: 50,
+          width: 250,
+          borderRadius: 20,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}>
+        <Text>Get Started</Text>
+      </View>
     </View>
   );
 };
@@ -17,8 +31,20 @@ const GetStarted = () => {
 export default GetStarted;
 
 const styles = StyleSheet.create({
-  mainScreen: {flex: 1, paddingHorizontal: 20, justifyContent: 'center'},
+  mainScreen: {
+    flex: 1,
+    paddingHorizontal: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
   heroImage: {height: 225, width: '100%', resizeMode: 'stretch'},
-  titleContainer: {},
-  titleText: {},
+  titleContainer: {
+    marginTop: 50,
+  },
+  titleText: {
+    fontSize: 30,
+    color: colors.primary,
+    textAlign: 'center',
+    fontFamily: fonts.SemiBold,
+  },
 });

15d243fcd060e87b9dd4b6052ec3f9158ed6357f
Raihan Sarkar <bdyoungdeveloper@gmail.com>
2022-06-12 21:39:58 +0600

    all changes update

--- src/screens/Categories/index.js
+++ src/screens/Categories/index.js
@@ -6,7 +6,7 @@ import {
   StatusBar,
   ScrollView,
 } from 'react-native';
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import {colors, DoctorImg, fonts} from '../../res';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {BoxItemTopProduct, Header} from '../../components/molecules';
@@ -14,6 +14,18 @@ import WhiteSpace from '../../components/atoms/WhiteSpace';
 
 const Categories = ({route, navigation}) => {
   const isDarkMode = useColorScheme() === 'dark';
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const url = 'https://jsonplaceholder.typicode.com/users';
+  useEffect(() => {
+    fetch(url).then(res => {
+      res
+        .json()
+        .then(data => setUsers(data))
+        .catch(error => console.log(error))
+        .finally(() => setLoading(false));
+    });
+  }, []);
   const categoriesData = [
     {
       name: 'Mahibur Rahman',
@@ -110,6 +122,45 @@ const Categories = ({route, navigation}) => {
                 />
               );
             })}
+          </View>
+          <View>
+            {loading ? (
+              <Text>Loadign ....</Text>
+            ) : (
+              <View>
+                {users.map((user, index) => (
+                  <View
+                    key={index}
+                    style={{
+                      marginVertical: 10,
+                      display: 'flex',
+                      alignItems: 'center',
+                      justifyContent: 'center',
+                    }}>
+                    <View
+                      style={{
+                        padding: 15,
+                        backgroundColor: colors.primary,
+                        borderRadius: 10,
+                        shadowColor: colors.primary,
+                        shadowOffset: {
+                          width: 0,
+                          height: 8,
+                        },
+                        shadowOpacity: 0.44,
+                        shadowRadius: 10.32,
+
+                        elevation: 16,
+                      }}>
+                      <Text>Id:{user.id}</Text>
+                      <Text>Name:{user.name}</Text>
+                      <Text>Email:{user.email}</Text>
+                      <Text>Address:{user.address?.street}</Text>
+                    </View>
+                  </View>
+                ))}
+              </View>
+            )}
           </View>
         </ScrollView>
       </View>
